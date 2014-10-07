0dd781dd71dbbf1fe17b12e9576c1fd311ab672a
BrainStarr <brian.r.starr@gmail.com>
2014-10-06 23:36:06 -0700

    Initial Commit

---
+++ Protocol Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Protocol Challenge Solution
+//
+//  Created by Brian Starr on 10/6/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Protocol Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Protocol Challenge Solution
+//
+//  Created by Brian Starr on 10/6/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
